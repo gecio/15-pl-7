ddeefb8f5de79d9e4b5fc2f3f7f0e70bc3623797
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2015-03-09 15:28:04 +0100

    Struktura projektow

---
+++ Source/ComputationalCluster.NetModule.Tests/NetModuleTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ComputationalCluster.NetModule;
+
+namespace ComputationalCluster.NetModule.Tests
+{
+    [TestFixture]
+    public class NetModuleTests
+    {
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(8)]
+        public void NetModule_Connect_ShouldConnect(int p)
+        {
+            var netModule = new NetModule();
+            netModule.Connect("127.0.0.1");
+            Assert.IsTrue(netModule.IsConnected);
+        }
+    }
+}
---
+++ Source/ComputationalCluster.NetModule/NetModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalCluster.NetModule
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NetModule
+    {
+        public NetModule()
+        {
+        }
+
+        public void Connect(string IP)
+        {
+        }
+
+        public bool IsConnected
+        {
+            get { return true; }
+        }
+    }
+}
